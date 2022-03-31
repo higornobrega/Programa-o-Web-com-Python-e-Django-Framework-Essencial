e8833907a5206dfe1b26358a30a203d0b4fdfc7f
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-03-30 23:16:18 -0300

    primeiro commit

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import contato, index, produto
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contato/', contato, name='contato'),
+    path('produto/', produto, name='produuto'),
+]
