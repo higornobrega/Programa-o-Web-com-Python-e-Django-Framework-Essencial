39df34dab909499cf14f26358181afd9b366d47a
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-04-22 18:45:25 -0300

    models part1

--- fusion/core/models.py
+++ fusion/core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+#SIGNALS
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+
+#Classe Abstrata não é escrita no BD
+class Base(models.Model):
+    criado = models.DateField('Data de Criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )    
+    servico = models.CharField("Serviço", max_length=100)
+    descricao = models.TextField("Descrição")
+    icone = models.CharField("Icone", max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return str(self.servico) 
+
+class Cargo(Base):
+    cargo = models.CharField("Cargo", max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+    def __str__(self):
+        return str(self.cargo)
+
+class Funcionario(Base):
+    nome = 
+
+
+
+
