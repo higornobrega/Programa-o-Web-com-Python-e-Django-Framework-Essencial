bf2669ec49a825e4d73f1d8ef706b5f74a7b1ab1
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-04-23 23:51:51 -0300

    53. Definindo os models

--- fusion/core/models.py
+++ fusion/core/models.py
@@ -1,6 +1,14 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
 from django.db import models
 from stdimage.models import StdImageField
 
+#Nomes diferentes para mídias
+import uuid #Gera Strings aleatórias
+def get_file_path(_intance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
 #SIGNALS
 from django.db.models import signals
 from django.template.defaultfilters import slugify
@@ -40,12 +48,22 @@ class Cargo(Base):
     class Meta:
         verbose_name = 'Cargo'
         verbose_name_plural = 'Cargos'
+
     def __str__(self):
         return str(self.cargo)
 
 class Funcionario(Base):
-    nome = 
+    nome = models.CharField('Nome', max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.TextField('Bio')
+    image = StdImageField('Image', upload_to=get_file_path, variations={'thumb' : {'width':480, 'height':480}})
+    facebook = models.CharField("Facebook", max_length=100, default='#')
+    twitter = models.CharField("twitter", max_length=100, default='#')
+    instagram = models.CharField("instagram", max_length=100, default='#')
 
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
 
-
-
+    def __str__(self):
+        return str(self.nome)
