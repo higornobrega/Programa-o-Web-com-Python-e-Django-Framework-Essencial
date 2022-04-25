943525ff18ff9db348df661a1257d8ed03488284
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-04-24 22:25:40 -0300

    54. Conhecendo e utilizando o Context Manager

--- fusion/core/views.py
+++ fusion/core/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
-
+from .models import Funcionario, Servico
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs) #Pega tudo que vier do contexto da página
+        context['servicos'] = Servico.objects.order_by('?').all() #Adicionar ao contexto
+        context['funcionarios'] = Funcionario.objects.order_by('?').all() #Adiciona ao contexto
+        return context
 
