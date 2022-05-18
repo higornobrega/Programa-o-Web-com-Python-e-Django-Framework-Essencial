f33c410ca923584fb2b3e3fc91789f9ecc1e2e30
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-05-18 12:31:43 -0300

    Testando model

--- fusion/core/tests/test_models.py
+++ fusion/core/tests/test_models.py
@@ -12,3 +12,31 @@ class GetFilePathTestCase(TestCase):
     def test_get_file_path(self):
         arquivo = get_file_path(None, 'teste.png')
         self.assertTrue(len(arquivo), len(self.filename))
+
+class ServicoTestCase(TestCase):
+    def setUp(self):
+        self.servico = mommy.make('Servico')
+    
+    def test_str(self):
+        self.assertEquals(str(self.servico), self.servico.servico)
+
+class CargoTestCase(TestCase):
+    def setUp(self):
+        self.cargo = mommy.make('Cargo')
+
+    def test_str(self):
+        self.assertEquals(str(self.cargo), self.cargo.cargo)
+
+class FuncionarioTestCase(TestCase):
+    def setUp(self):
+        self.funcionario = mommy.make('Funcionario')
+
+    def test_str(self):
+        self.assertEquals(str(self.funcionario), self.funcionario.nome)
+
+class FeatureTestCase(TestCase):
+    def setUp(self):
+        self.feature = mommy.make('Feature')
+
+    def test_str(self):
+        self.assertEquals(str(self.feature), self.feature.titulo)
