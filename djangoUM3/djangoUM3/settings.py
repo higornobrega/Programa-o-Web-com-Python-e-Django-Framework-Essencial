3f4fec18455ab1615e7e89c00ae8c92f21620ecc
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-05-19 15:31:13 -0300

    teste

--- djangoUM3/djangoUM3/settings.py
+++ djangoUM3/djangoUM3/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c#ftme38755r%*0@5-r&7v+#ap!a7=p#gq2vp1=f@gz)8x&$l-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
